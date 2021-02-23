ba33445f9c42a1069e60105a3322a26a6c3843cb
Kanoj Kaushik Vora <kkvsith@gmail.com>
2021-02-23 23:39:24 +0000

    react-landing

---
+++ src/components/navbar/NavbarElements.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+import { Link as LinkR } from "react-router-dom";
+// import { Link as LinkS } from "react-scroll";
+
+
+
+export const NavbarContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    height: 80px;
+    z-index: 1;
+    width: 100%;
+    padding: 0 24px;
+    max-width: 1100px;
+`;
+
+export const NavLogo = styled(LinkR)`
+    color: red;
+    justify-self: flex-start;
+    cursor: pointer;
+    font-size: 1.5rem;
+    display: flex;
+    align-items: center;
+    margin-left: 24px;
+    font-weight: bold;
+    text-decoration: none;
+`;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavbarContainer, NavLogo } from "./NavbarElements";
+
+const Navbar = () => {
+    return (
+        <div>
+
+                <NavbarContainer>
+                    <NavLogo to="/">Land</NavLogo>
+                </NavbarContainer>
+           
+        </div>
+    )
+}
+
+export default Navbar
